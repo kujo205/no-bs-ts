1c5f83d70641e725a127c1803717a4a55b8e141c
kujo205 <kuc8301@gmail.com>
2023-02-12 14:08:31 +0200

    2nd task

--- dist/task2.js
+++ dist/task2.js
@@ -10,14 +10,14 @@
 //     },undefined)
 // }
 // forEach([1,2,3,4,5,6],(e)=>{console.log(`forEach ${e}`)})
-function map(array, mapCallBack) {
-    let mappedArray = [];
-    array.reduce((p, c) => {
-        mappedArray.push(mapCallBack(c));
-    }, []);
-    return mappedArray;
-}
-console.log(map([1, 2, 3, 4, 5, 6], (el) => el * 2));
+// function map<T, K>(array: T[], mapCallBack: (value: T) => K): K[] {
+//   let mappedArray: K[] = [];
+//   array.reduce((p: any, c) => {
+//     mappedArray.push(mapCallBack(c));
+//   }, [] as K[]);
+//   return mappedArray;
+// }
+// console.log(map([1, 2, 3, 4, 5, 6], (el) => el * 2));
 // function filter<T>(array: T[], filterCallBack: (value: T) => boolean):T[] {
 //   let filteredArray: any[] = [];
 //   array.reduce((p: any, c, i) => {
